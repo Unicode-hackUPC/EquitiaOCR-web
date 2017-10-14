f3921d3e32cb723a71a00c81bdcdda93b21398f4
Alexa Noel <alexanowel@live.fr>
2017-10-14 12:17:38 +0200

    Cognitive vision image analysis

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 
 import Header from './components/Header'
 import Home from './components/Home'
+import analyseImage from './services/cognitiveServices.js'
 
 import './style/index.css';
 
 class App extends Component {
   render() {
+      analyseImage("https://scontent-mad1-1.xx.fbcdn.net/v/t34.0-12/22497400_10211204530124478_179622941_n.jpg?oh=471806aef54ba3f54130d178ba36391a&oe=59E44C8B")
     return (
       <div className="App">
         <Header />
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,9 @@ class Home extends Component {
   constructor(props){
     super(props);
     this.state = {
+      fileUploaded: false
     }
   }
-
-  convertToHtml(inFile) => {
-  }
-
   componentDidMount(){
   }
 
@@ -19,7 +16,7 @@ class Home extends Component {
         <div className="draganddrop">
         </div>
         <div className="buttons">
-          <button className="button">Upload a picture</button>
+          <button className="button" >Upload a picture</button>
           <button className="button">Take a picture</button>
         </div>
       </div>
