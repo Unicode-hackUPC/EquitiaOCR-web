3e0157a2cd043e906396c9a0f841ba092b3bc999
flotos <flotos@hotmail.fr>
2017-10-14 14:49:59 +0100

    cognitiveService now handle both Url and File (file not tested)

--- src/services/cognitiveServices.js
+++ src/services/cognitiveServices.js
@@ -1,47 +1,62 @@
-const cognitiveServices = require('cognitive-services')
+const cognitiveServices = require('cognitive-services');
 const client = cognitiveServices.computerVision({
-  apiKey: "c11489d328804ca797a22451ae944c39",
-  endpoint: "westcentralus.api.cognitive.microsoft.com"
+  apiKey: 'c11489d328804ca797a22451ae944c39',
+  endpoint: 'westcentralus.api.cognitive.microsoft.com',
 });
-const headers = {'Content-type': 'application/json' }
 
-export default function analyseImage(fileUrl) {
-
-  let isRunning = true;
-
-  const body = {url: fileUrl}
+export function analyseImageByUrl(fileUrl) {
+  const headers = { 'Content-type': 'application/json' };
+  const body = { url: fileUrl };
   let parameters = {
-    "handwriting": true,
+    handwriting: true,
   };
 
-  client.recognizeHandwrittenText({
-    parameters,
-    headers,
-    body
-  })
-  .then((operationId) => {
-    parameters = {
-      'operationId': operationId
-    };
-    const interval = setInterval(() => {
-      console.log("wesh")
-      if(isRunning) {
-        return client.getHandwrittenTextOperationResult({
-          parameters
-        }).then((response) => {
-          if (response.status !== "Running") isRunning = false;
-          console.log("response", response);
-        })
-        .catch((err) => {
-          console.log('err', err);
-        });
-      } else {
-          clearInterval(interval);
-      }
-    }, 500)
-
-    return client.getHandwrittenTextOperationResult({
-      parameters
-    });
-  })
+  sendRequest(headers, body, parameters);
 }
+
+export function analyseImageByFile(fileBuf) {
+  const headers = { 'Content-type': 'application/octet-stream' };
+  const body = fileBuf;
+  let parameters = {
+    handwriting: true,
+  };
+
+  sendRequest(headers, body, parameters);
+}
+
+const sendRequest = (headers, body, parameters) => {
+  let isRunning = true;
+
+  client
+    .recognizeHandwrittenText({
+      parameters,
+      headers,
+      body,
+    })
+    .then(operationId => {
+      parameters = {
+        operationId: operationId,
+      };
+      const interval = setInterval(() => {
+        if (isRunning) {
+          return client
+            .getHandwrittenTextOperationResult({
+              parameters,
+            })
+            .then(response => {
+              if (response.status !== 'Running') isRunning = false;
+              console.log('response', response);
+            })
+            .catch(err => {
+              console.log('err', err);
+            });
+        } else {
+          clearInterval(interval);
+        }
+      }, 500);
+
+      return client.getHandwrittenTextOperationResult({
+        parameters,
+      });
+    });
+};
